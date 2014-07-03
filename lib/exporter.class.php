c19f2194feded8a6b8988615addd7848a1047041
Dáithí Coombes <raremac3@raremac3s-computer.local>
2014-07-03 13:55:10 +0100

    loop through query in batches

--- lib/exporter.class.php
+++ lib/exporter.class.php
@@ -2,14 +2,23 @@
 /**
  * Export data from mysql table
  *
+ * @usage:
+ * <code>
+ * </code>
  * @author daithi coombes <webeire@gmail.com>
  */
 class Exporter{
 
+	/* @var integer The max rows to return before making new query */
+	private $batch_size = 10000;
+	/* @var Database The database instance */
+	private $db;
 	/* @var array An array of Error instances. Empty array if none */
 	private $errors = array();
-	/* @var integer The max rows to return before making new query */
-	private $limit = 10000;
+	/* @var integer The max results to return */
+	private $max_results;
+	/* @var array An array of mysql records */
+	private $records = array();
 	/* @var string The sql query to run. @see Exporter::set_sql() */
 	private $sql;
 	/* @var resource File handle returned by tmpfile(). 
@@ -22,48 +31,97 @@ class Exporter{
 
 	/**
 	 * Factory method
-	 * @return Exporter Returns new Exporter instance
+	 * @param  array  $params An array of param=>value pairs
+	 * @return Exporter         Returns new Exporter instance
 	 */
-	public function factory(){
+	public function factory( $params=array() ){
 
-		return new Exporter();
+		$obj = new Exporter();
+
+		if( count($params) )
+			return $obj->set( $params );
+
+		return $obj;
 	}
 
-	public function set( $param, $value ){
+	/**
+	 * Batch process.
+	 * Writes records to tmpfile
+	 * @param  integer $start   Default 0. LIMIT start value
+	 * @return Exporter           Returns self for chaining
+	 */
+	public function batch( $start=0 ){
 
-		//if setter for this param
-		if( method_exists($this, "set_{$param}") ){
+		//vars
+		$this->start = $start;
+		$this->end = (int) $start + (int) $this->batch_size;
+		
+		//set the LIMIT statement
+		$this->sql_set_limit( $this->start, $this->end );
 
-			$method = "set_{$param}";
-			return $this->$method( $value );
-		}
+		//run query
+		$this->db->query( $this->sql );
+		while( $row = $this->db->fetchArray('array') )
+			$this->records[] = $row;
 
-		//else set directly
-		$this->$param = $value;
+		//need to run more?
+		if( 
+			(count($this->records) <= $this->batch_size) ||
+			($this->max_results && (count($this->records) < $this->max_results))
+		)
+			$this->batch( $this->end, $this->records );
+
+		//trim records if max_results set
+		if( $this->max_results )
+			$this->records = array_slice($this->records, 0, $this->max_results);
+
 		return $this;
 	}
 
-	public function run( Database $db, $start=0 ){
+	/**
+	 * Get a params value
+	 * @param  string $param The param to get
+	 * @return mixed        The param value
+	 */
+	public function get( $param ){
 
-		$args = func_get_args();
+		return $this->$param;
+	}
 
-		$total = $this->get_total( $db );
+	/**
+	 * Set a param.
+	 * If the private method set_$param exists then it will be executed
+	 * @param string|array $params An array of param=>value pairs or param name
+	 * @param mixed $value Optional. If $param is string pass value here.
+	 * @return returns this for chaining
+	 */
+	public function set( $params, $value=null ){
 
-		$records = array();
-		$end = (int) $start + (int) $this->limit;
-		$sql = $this->sql . "\nLIMIT {$start}, {$end}";
+		//array of params
+		if( is_array($params) ){
 
+			foreach( $params as $param=>$value)
+				if( method_exists($this, "set_{$param}") ){
 
-		$db->query( $this->sql );
-		while( $row = $db->fetchArray('array') )
-			$records[] = $row;
+					$method = "set_{$param}";
+					$this->$method( $value );
+				}
+				else
+					$this->$param = $value;
+		}
 
-		$start = $end;
+		//else set directly
+		else{
 
-		if( count($records)>=$this->limit )
-			$records = array_merge( $records, $this->run($db, $end) );
+			if( method_exists($this, "set_{$params}") ){
 
-		return $records;
+				$method = "set_{$params}";
+				$this->$method( $value );
+			}
+			$this->$params = $value;
+		}
+
+		return $this;
 	}
 
 	/**
@@ -75,15 +133,38 @@ class Exporter{
 		return tmpfile();
 	}
 
-	private function get_total( Database $db ){
+	/**
+	 * Sets the database.
+	 * Use $this->set( 'db', Database )
+	 * @param Database $db The database instance
+	 */
+	private function set_db( Database $db ){
 
-		$sql = preg_replace('/^SELECT\s/i', 'SELECT SQL_CALC_FOUND_ROWS ', $this->sql);
+		$this->db = $db;
+	}
 
-		$db->query( $sql );
-		$db->query( 'SELECT FOUND_ROWS()' );
-		while( $row = $db->fetchArray('array') )
-			$res[] = $row;
+	/**
+	 * Sets the LIMIT statement
+	 * @param  integer $start The start
+	 * @param  integer $end   The end
+	 * @return Exporter        Returns self for chaining
+	 */
+	private function sql_set_limit( $start, $end ){
 
-		return $res;
+		$pattern = '/LIMIT\s+([0-9]+).+\s([0-9]+)/im';
+		$statement = "LIMIT {$start}, {$end}";
+
+		//search for LIMIT statement
+		preg_match($pattern, $this->sql, $matches);
+
+		//replace current limit
+		if( count($matches)>1 )
+			$this->sql = preg_replace($pattern, $statement, $this->sql);
+
+		//else append LIMIT statement
+		else
+			$this->sql .= $statement;
+
+		return $this;
 	}
 }
